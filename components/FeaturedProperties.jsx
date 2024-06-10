b6a0154f9310bf429e4394e930fda2381ea1bbfb
Cinderhelm <itorch100@gmail.com>
2024-06-10 22:53:13 +0300

    another try

--- components/FeaturedProperties.jsx
+++ components/FeaturedProperties.jsx
@@ -4,24 +4,20 @@ import FeaturedPropertyCard from "./FeaturedPropertyCard";
 async function FeaturedProperties() {
   const properties = await fetchProperties({ showFeatured: true });
 
-  console.log(properties);
-
-  return (
-    properties.length > 0 && (
-      <section className="bg-blue-50 px-4 pt-6 pb-10">
-        <div className="container-xl lg:container m-auto">
-          <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
-            Featured Properties
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {properties.map((property) => (
-              <FeaturedPropertyCard property={property} key={property._id} />
-            ))}
-          </div>
+  return properties.length > 0 ? (
+    <section className="bg-blue-50 px-4 pt-6 pb-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+          Featured Properties
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {properties.map((property) => (
+            <FeaturedPropertyCard property={property} key={property._id} />
+          ))}
         </div>
-      </section>
-    )
-  );
+      </div>
+    </section>
+  ) : null;
 }
 
 export default FeaturedProperties;
